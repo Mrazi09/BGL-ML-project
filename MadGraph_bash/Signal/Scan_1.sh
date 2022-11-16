342ff799cbcf37c2a086f3c33f947d748b76d3b8
João Pino Gonçalves <35638118+Mrazi09@users.noreply.github.com>
2022-11-16 17:33:46 +0000

    Delete MadGraph_bash directory

--- MadGraph_bash/Signal/Scan_1.sh
+++
@@ -1,7 +0,0 @@
-!cp /home/jpedropino@UA.PT/Workstation/MadGraph/MG5_aMC_v2_7_0/1-BGL_signal/Cards/pythia8_card_default.dat /home/jpedropino@UA.PT/Workstation/MadGraph/MG5_aMC_v2_7_0/1-BGL_signal/Cards/pythia8_card.dat
-!cp /home/jpedropino@UA.PT/Workstation/MadGraph/MG5_aMC_v2_7_0/1-BGL_signal/Cards/delphes_card_ATLAS.dat /home/jpedropino@UA.PT/Workstation/MadGraph/MG5_aMC_v2_7_0/1-BGL_signal/Cards/delphes_card.dat
-launch 1-BGL_signal
-3
-set nevents 100000
-set ebeam1 7000
-set ebeam2 7000
